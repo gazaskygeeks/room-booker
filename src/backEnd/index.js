f77e88d598420c9cf5437b1f62234adf0e72b767
shahenaz Monia <shahy.m.93@gmail.com>
2017-04-18 16:24:54 +0300

    build express server and react enviroment

---
+++ src/backEnd/index.js
@@ -0,0 +1,9 @@
+
+var express = require('express');
+const app = express();
+
+app.use('/',express.static('public'));
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000'); // eslint-disable-line
+});
---
+++ src/frontEnd/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+const App = () => <div>Hi</div>;
+
+reactDOM.render(<App/>, document.getElementById('myApp'));
