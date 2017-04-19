9bb268899de100f8c3f22370ec0572e9bd3b6bb5
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-19 10:27:29 +0300

    remove console log

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -5,5 +5,4 @@ app.use(express.static('public'));
 
 app.listen(3000, () => {
   console.log('App listening on port 3000'); // eslint-disable-line no-console
-  console.log('App listening on port 3000'); // eslint-disable-line no-console
 });
