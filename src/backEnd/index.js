4f00ddb099435efcfa49932bd7c94aa9f4e554a7
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-20 16:53:06 +0300

    call createtable fun and call database config

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -3,7 +3,17 @@ const app = express();
 <<<<<<< Updated upstream
 const port = process.env.PORT || 8080;
 
+=======
+const config = require('../database/config.js');
+const cl = require('../database/client.js');
+const client = cl(config);
+const createtable = require('../database/createTable.js');
+createtable(client);
+>>>>>>> Stashed changes
 app.use(express.static('public'));
+createtable(client);
+
+
 
 app.listen(port, () => {
   console.log('Our app is running on http://localhost:' + port); // eslint-disable-line no-console
