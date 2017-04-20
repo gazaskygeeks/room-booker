88d81e918ca048654e1a896ebc7d9a3a59c3f380
Muhammed Mushtaha <muhamed.mushtaha@outlook.com>
2017-04-20 11:37:23 +0300

    added heroku PORT to express server

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App listening on port 3000'); // eslint-disable-line no-console
 });
