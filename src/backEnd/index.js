87cfc4838d933d4038401f4defc31f035b6af2bf
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-23 12:24:48 +0300

    run server after run Migration and add body-parser and remove conflict

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
-<<<<<<< Updated upstream
 const port = process.env.PORT || 8080;
-
-=======
 const config = require('../database/config.js');
 const cl = require('../database/client.js');
 const client = cl(config);
-const createtable = require('../database/createTable.js');
-createtable(client);
->>>>>>> Stashed changes
+const routes = require('./routes.js');
+const runMigrations = require('../database/createTable.js');
+const bodyParser = require('body-parser');
+
 app.use(express.static('public'));
-createtable(client);
-
-
-
-app.listen(port, () => {
-  console.log('Our app is running on http://localhost:' + port); // eslint-disable-line no-console
+app.use(bodyParser.json());
+app.use(routes);
+runMigrations(client,(err)=>{
+  if(err)throw err;
+  app.listen(port, () => {
+    console.log('Our app is running on http://localhost:' + port); // eslint-disable-line no-console
+  });
 });
