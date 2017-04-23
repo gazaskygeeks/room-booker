b64997eaf495bf50ae65422eb3d94444dbd0f3ae
shahenaz Monia <shahy.m.93@gmail.com>
2017-04-23 15:13:41 +0300

    remove the confict in index.js

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
-<<<<<<< Updated upstream
+
 const port = process.env.PORT || 8080;
 
-=======
 const config = require('../database/config.js');
 const cl = require('../database/client.js');
 const client = cl(config);
 const createtable = require('../database/createTable.js');
 createtable(client);
->>>>>>> Stashed changes
 app.use(express.static('public'));
 createtable(client);
 
