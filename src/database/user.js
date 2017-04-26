923356b60a770284054fa94450a1adadee83b92a
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-26 10:39:01 +0300

    prevent queries from sqlinjection

--- src/database/user.js
+++ src/database/user.js
@@ -3,14 +3,14 @@ const cl = require('./client.js');
 const client = cl(config);
 
 const insertUser = (data,cb)=>{
-  const sqlQuery = `INSERT INTO users(email,priveleges,first_name,last_name)VALUES('${data.email}','1','${data.firstName}','${data.lastName}')`;
-  client.query(sqlQuery,(err,result)=>{
+  const sqlQuery = 'INSERT INTO users(email,privileges,first_name,last_name)VALUES($1,$2,$3,$4)';
+  client.query(sqlQuery,[data.email,'1',data.firstName,data.lastName],(err,result)=>{
     cb(err,result);
   });
 };
-const selectUser = (data,cb)=>{
-  const sqlQuery = `SELECT email,first_name,last_name from users WHERE email='${data.email}'`;
-  client.query(sqlQuery,(err,result)=>{
+const selectUser = (email,cb)=>{
+  const sqlQuery = 'SELECT id,email,first_name,last_name from users WHERE email=$1';
+  client.query(sqlQuery,[email],(err,result)=>{
     cb(err,result);
   });
 };
