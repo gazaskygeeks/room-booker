17bc45bb1c023ea17351d18889a6cc4af5905107
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-26 10:29:24 +0300

    complete auth operation and set user id as cookie

--- src/backEnd/controllers/usersControllers.js
+++ src/backEnd/controllers/usersControllers.js
@@ -2,29 +2,26 @@ const utils = require('../utils/utils.js');
 const usersdb = require('../../database/user.js');
 
 module.exports = {
-  insertUser: function() {
-    //do something
-  },
   checkAuthThenInsert: function(req, res) {
-    utils.checkAuth(req.body.accessToken,(authentication)=>{
-      if(authentication){
-        const valid = utils.validEmail(req.body);
-        if(valid){
-          usersdb.insertUser(req.body,(err,result)=>{
-            console.log(result);
-            usersdb.selectUser(req.body,(err,result)=>{
-            console.log(result);
-            });
+    utils.checkAuth(req.body.accessToken)
+      .then(validEmail => {
+        if (validEmail) {
+          usersdb.selectUser(validEmail, (err, result) => {
+            if (result.rowCount === 0) {
+              usersdb.insertUser(req.body,()=>{
+                usersdb.selectUser(req.body.email,(err,result)=>{
+                  res.cookie('remember',result.rows[0].id, { expires: new Date(Date.now() + 900000000000),signed: true });
+                  if(err) res.status(404).end();
+                  res.status(200).end();
+                });
+              });
+            }
+          });
+        } else {
+          res.json({
+            'status': 'bad email'
           });
         }
-        else {
-          res.end(JSON.stringify({'valid':false}));
-        }
-      }
-      else{
-        res.end(JSON.stringify({'valid':false}));
-      }
-    });
-
+      });
   }
 };
