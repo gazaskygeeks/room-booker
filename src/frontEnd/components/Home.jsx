aa9395ad10e1ddbe204297b3619f344e07cb118a
abeeralshaer <abber2000g@gmail.com>
2017-04-26 06:03:32 -0500

    add onClick prop

--- src/frontEnd/components/Home.jsx
+++ src/frontEnd/components/Home.jsx
@@ -1,24 +1,26 @@
-import React,{Component} from 'react';
+import React from 'react';
 import {ListGroup, ListGroupItem} from 'react-bootstrap';
+import {PropTypes} from 'prop-types';
+import GoogleLogin from './GoogleLogin.jsx';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    return (
+
+const Home = (props) =>(
       <div>
         <h1>Room Booking</h1>
         <h3>Gaza Sky Geeks</h3>
         <ListGroup>
-          <ListGroupItem href="#meetingRoom1">Meeting Room 1</ListGroupItem>
+          <ListGroupItem href="#meetingRoom1" onClick={()=>{props.onClick('ROOM_AVAILABITLITY');}}>Meeting Room 1</ListGroupItem>
           <ListGroupItem href="#meetingRoom2">Meeting Room 2</ListGroupItem>
           <ListGroupItem href="#meetingRoom3">Meeting Room 3</ListGroupItem>
           <ListGroupItem href="#meetingRoom4">Meeting Room 4</ListGroupItem>
         </ListGroup>
+        <GoogleLogin/>
       </div>
     );
-  }
-}
+
+Home.propTypes = {
+  onClick : PropTypes.func.isRequired
+};
+
 export default Home;
