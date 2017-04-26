c7a1c0ee9d7ef221c421dd692db739b97f82cd12
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-26 10:34:24 +0300

    replace axios with fetch and take only email as validEmail parameter

--- src/backEnd/utils/utils.js
+++ src/backEnd/utils/utils.js
@@ -1,14 +1,21 @@
-var axios = require('axios');
-const checkAuth = (accessToken,cb)=>{
-  axios.get('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token='+accessToken).then(result=>{
-    cb(result.data.email_verified) ;
-  });
+const fetch = require('node-fetch');
+
+const checkAuth = (accessToken) => {
+  return fetch('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + accessToken)
+    .then(res=>res.json())
+    .then(result => {
+      if (result.email && validEmail(result.email)) {
+        return result.email;
+      } else {
+        return false;
+      }
+    });
 };
-const validEmail = (data)=>{
-  return (data.email.includes('mhmd')||data.email.includes('@mercycorps.com'));
+const validEmail = (email) => {
+  return email.includes('mhmd') || email.includes('@mercycorps.com');
 };
 
 module.exports = {
-  checkAuth:checkAuth,
-  validEmail:validEmail
+  checkAuth: checkAuth,
+  validEmail: validEmail
 };
