c6d521d47d13efba00c76cadf6e293356db33b70
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-27 11:24:00 +0300

    set db test environment with pool

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -1,33 +1,50 @@
-const dbConfig = require('./config.js');
+const pg = require('pg');
+var nock = require('nock');
 const async = require('async');
-const connect = require('./client.js');
-const createTables = require('./createTable.js');
-const client = connect(dbConfig);
+const dbConfig = require('./config.js');
+const setupClient = new pg.Client(Object.assign(dbConfig, {
+  database: 'postgres'
+}));
 
-const AllDatabaseQueries = (client, cb) => {
+const pool = require('./pool.js');
+const createTables = require('./createTable.js');
+
+const request = require('supertest');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const routes = require('../backEnd/routes.js');
+
+app.use(bodyParser.json());
+app.use(routes);
+
+
+beforeAll(done => {
   async.series([
-    function(callback) {
-      client.query('CREATE DATABASE testRoomBooking', callback);
-    },
-    function(callback) {
-      createTables(client, callback);
-    },
-    (callback) => {
-      expect(1+2).toEqual(3);
-      callback();
-    },
-    function(callback) {
-      client.query('DROP DATABASE testRoomBooking', callback);
-    }
-  ], cb);
+    cb =>
+      setupClient.connect(cb),
+    cb =>
+      setupClient.query('DROP DATABASE IF EXISTS test_room_booking', cb),
+    cb =>
+      setupClient.query('CREATE DATABASE test_room_booking', cb)
+    ],
+    err => {
+      if (err) throw err;
+      done();
+    });
+});
+
+afterAll(done => {
+  setupClient.query('DROP DATABASE test_room_booking', err => {
+    if (err) throw err;
+    setupClient.end();
+    pool.end();
+    done();
+  });
+});
+
+
 };
 
-test('integration tests',()=>{
-  AllDatabaseQueries(client, (err) => {
-    if (err) {
-      throw err;
-    }
 
-    client.end();
-  });
 });
