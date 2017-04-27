72f2dc3b2aa1981df55d9efd09945d5e502663ec
Muhammed Mushtaha <muhamed.mushtaha@outlook.com>
2017-04-27 15:10:50 +0300

    Modify bookings reducer structure

--- src/frontEnd/index.jsx
+++ src/frontEnd/index.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import reactDOM from 'react-dom';
-import {container} from 'react-bootstrap';
 import store from './store.js';
 import {Provider} from 'react-redux';
 import App from './components/containers/App.js';
 
 const Main = () => {
   return (
-        <Provider store={store} >
-            <container id="main">
-                <App/>
-            </container>
-        </Provider>
+    <Provider store={store}>
+      <div className="container" id="main">
+        <App/>
+      </div>
+    </Provider>
   );
 };
 
 reactDOM.render(
-    <Main/>, document.getElementById('myApp'));
+  <Main/>, document.getElementById('myApp'));
