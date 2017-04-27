9e9179ce2421ae9a6c920a28dc39e36a41d2363c
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-27 17:30:54 +0300

    refactor code and create function to set cookie and combine test for token and validation

--- src/backEnd/controllers/usersControllers.js
+++ src/backEnd/controllers/usersControllers.js
@@ -1,41 +1,36 @@
 const utils = require('../utils/utils.js');
 const usersdb = require('../../database/user.js');
+
+const respondWithUser = (res, user) => {
+  res.cookie('remember', user.id, {
+    expires: new Date(Date.now() + 900000000000),
+    signed: true
+  });
+  res.json(user);
+};
+
 module.exports = {
-  checkAuthThenInsert: function(req, res) {
-    utils.checkAuth(req.body.accessToken)
+  checkAuthThenInsert: (req, res) => {
+    const { firstName, lastName, accessToken } = req.body;
+    utils.checkAuth(accessToken)
       .then(email => {
-        if (email) {
-          const validEmail = utils.validEmail(email);
-          if (validEmail) {
-            usersdb.selectUser(validEmail, (err, result) => {
-              if (err) throw err;
-              if (result.rowCount === 0) {
-                usersdb.insertUser(req.body, () => {
-                  usersdb.selectUser(req.body.email, (err, result) => {
-                    res.cookie('remember', result.rows[0].id, {
-                      expires: new Date(Date.now() + 900000000000),
-                      signed: true
-                    });
-                    res.status(200).end(JSON.stringify(result.rows));
-                    if (err) res.status(404).end();
-                    res.status(200).end();
-                  });
-                });
-              } else {
-                res.json(result.rows);
-              }
-            });
-          } else {
-            res.status(407).json({
-              'error': 'unauthorized email'
-            });
-          }
+        if (email && utils.validEmail(email)) {
+          usersdb.selectUser(email, (err, user) => {
+            if (err) throw err;
+            if (user) {
+              respondWithUser(res, user);
+            } else {
+              usersdb.insertUser(email, firstName, lastName, (err, user) => {
+                if (err) throw err;
+                respondWithUser(res, user);
+              });
+            }
+          });
         } else {
-          res.status(400).json({
-            'error': 'bad email'
+          res.status(401).json({
+            'error': 'unauthorized'
           });
         }
       });
-
   }
 };
