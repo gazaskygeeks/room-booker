abdbc5031326c312d440f605d8afb2e00cde64dd
Muhammed Mushtaha <muhamed.mushtaha@outlook.com>
2017-04-27 15:07:24 +0300

    import Containers in App.jsx

--- src/frontEnd/components/App.jsx
+++ src/frontEnd/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ChangeView from './containers/ChangeView.js';
-import RoomAvailbility from './RoomAvailability.jsx';
+import ChangeDayView from './containers/ChangeDayView.js';
 import {PropTypes} from 'prop-types';
 
 const App = ({currentView}) => {
   switch (currentView) {
   case 'ROOM_1':
-    return <RoomAvailbility room={currentView}/>;
+    return <ChangeDayView room={currentView}/>;
   default:
     return <ChangeView/>;
   }
