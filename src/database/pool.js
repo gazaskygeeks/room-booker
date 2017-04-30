ddc29d9bfed2d385814a070bc5f724fca15c1914
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-04-30 09:41:20 +0300

    use detruction to get DB_CONFIG

--- src/database/pool.js
+++ src/database/pool.js
@@ -1,6 +1,6 @@
-const dbConfig = require('../../config.js').DB_CONFIG;
+const {DB_CONFIG} = require('../../config.js');
 const pg = require('pg');
-const pool = new pg.Pool(dbConfig);
+const pool = new pg.Pool(DB_CONFIG);
 
 pool.on('error', (err) => {
   if(err) throw err;
