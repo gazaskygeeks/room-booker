11062d1388d2e8f7513b438312cce31c9332b584
mhmd.shorafa <mhmd_shorafa@hotmail.com>
2017-05-02 16:03:32 +0000

    add room table

--- src/database/createTable.js
+++ src/database/createTable.js
@@ -19,6 +19,7 @@ module.exports = (cb) => {
       (callback) => {
         client.query(`CREATE TABLE IF NOT EXISTS bookings (
           id SERIAL PRIMARY KEY,
+          room_id varchar(255),
           calendar_id varchar(255),
           event_id varchar(255),
           users_id integer REFERENCES users(id),
@@ -26,6 +27,15 @@ module.exports = (cb) => {
           location varchar(255),
           start_date timeStamp,
           end_date timeStamp
+        )`,callback);
+      },
+      (callback) => {
+        client.query(`CREATE TABLE IF NOT EXISTS rooms (
+          id SERIAL PRIMARY KEY,
+          room_name varchar(255),
+          room_capacity varchar(255),
+          room_image varchar(255),
+          location varchar(255)
         )`,callback);
       }
     ], (err) => {
