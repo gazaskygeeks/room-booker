665b5c2e271b72e299bbc8501fb424cdd7a365e4
shahenaz Monia <shahy.m.93@gmail.com>
2017-05-03 12:11:35 +0300

    add routes for logout,getRooms, and getUserEvent

--- src/backEnd/routes.js
+++ src/backEnd/routes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const userControllers = require('./controllers/usersControllers.js');
 const bookingsControllers = require('./controllers/bookingsController.js');
-
+const roomsControllers = require('./controllers/roomsControllers.js')
 
 router
 .get('/profile', userControllers.getProfile )
 .post('/user', userControllers.checkAuthThenInsert)
 .get('/event',bookingsControllers.getAllEvents)
 .get('/events',bookingsControllers.createEvent)
+.get('/userevents',bookingsControllers.UserEvent)
+.get('/rooms',roomsControllers.getRooms)
+.get('/logout',userControllers.logout)
 .delete('/event',bookingsControllers.deleteEvent)
 ;
 
