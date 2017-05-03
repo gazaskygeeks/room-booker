e6407def0df34566b812c44d5b872cc746bca0a6
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-03 14:00:43 +0300

    return payload

--- src/frontEnd/reducers.js
+++ src/frontEnd/reducers.js
@@ -11,9 +11,7 @@ const currentUserReducer = (state={}, action) => {
 const bookings = (state = [] , action)=> {
   switch (action.type) {
   case 'FETCH_DAY_BOOKING':
-    return action.payload.filter((event) => {
-      if(event.created == '2017-04-26T10:29:10.000Z') return action.payload;
-    });
+    return action.payload;
   case 'FETCH_WEEK_BOOKING':
     return action.payload;
   default:return state;
