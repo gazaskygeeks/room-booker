72d2d58f0a6fb09603a67419a6e22a7576dbe8ab
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-03 13:58:09 +0300

    add onClick and props validation

--- src/frontEnd/components/Lobby.jsx
+++ src/frontEnd/components/Lobby.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import {PropTypes} from 'prop-types';
 import GoogleLogin from './GoogleLogin.jsx';
 
-const Home = (props) => (
+const Lobby = (props) => (
   <div>
     <div className="row">
       <h1 className="text-center">Room Booking</h1>
       <div className="list-group col-md-6 col-md-offset-3">
         <button type="button" onClick={() => {
-            props.onClick('ROOM_1_AVAILABILITY');
-            props.getEvent();
+          props.onClick('ROOM_1_AVAILABILITY');
+          props.getEvent();
         }} className="list-group-item">Meeting Room 1</button>
         <button type="button" onClick={() => {
-            props.onClick('ROOM_2_AVAILABILITY');}} className="list-group-item">Meeting Room 2</button>
-        <button type="button" onClick={() => {props.onClick('ROOM_3_AVAILABILITY');}} className="list-group-item">Meeting Room 3</button>
-        <button type="button" onClick={() => {props.onClick('ROOM_4_AVAILABILITY');}} className="list-group-item">Meeting Room 4</button>
+          props.onClick('ROOM_2_AVAILABILITY');
+        }} className="list-group-item">Meeting Room 2</button>
+        <button type="button" onClick={() => {
+          props.onClick('ROOM_3_AVAILABILITY');
+        }} className="list-group-item">Meeting Room 3</button>
+        <button type="button" onClick={() => {
+          props.onClick('ROOM_4_AVAILABILITY');
+        }} className="list-group-item">Meeting Room 4</button>
       </div>
     </div>
     <div className="row">
@@ -25,8 +30,9 @@ const Home = (props) => (
   </div>
 );
 
-Home.propTypes = {
-  onClick: PropTypes.func.isRequired
+Lobby.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  getEvent: PropTypes.func.isRequired
 };
 
-export default Home;
+export default Lobby;
