ae60cac22ce884cc3dc327a6de1a37032cb46b2d
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-03 11:33:43 +0300

    dispatch new action for login

--- src/frontEnd/components/App.jsx
+++ src/frontEnd/components/App.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
-import ChangeView from './containers/ChangeView.js';
+import Home from './containers/Lobby.js';
 import ChangeDayView from './containers/ChangeDayView.js';
 import {PropTypes} from 'prop-types';
-
-const App = ({currentView}) => {
-  switch (currentView) {
-  case 'ROOM_1':
-    return <ChangeDayView room={currentView}/>;
-  default:
-    return <ChangeView/>;
+import Tabs from './Tabs.jsx';
+import Spinner from './Spinner.jsx';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      isLoggedIn:props.isLoggedIn
+    };
   }
-};
+
+  componentDidMount(){
+    this.state.isLoggedIn();
+  }
+
+  render(){
+
+    switch (this.props.currentView) {
+    case 'ROOM_1_AVAILABILITY':
+      return <ChangeDayView room={this.props.currentView}/>;
+    case 'ROOM_2_AVAILABILITY':
+      return <ChangeDayView room={this.props.currentView}/>;
+    case 'ROOM_3_AVAILABILITY':
+      return <ChangeDayView room={this.props.currentView}/>;
+    case 'ROOM_4_AVAILABILITY':
+      return <ChangeDayView room={this.props.currentView}/>;
+    case 'HOME':
+      return <Tabs/>;
+    case 'LOBBY':
+      return <Home/>;
+    case 'SPINNER':
+      return <Spinner/>;
+    }
+  }
+}
+
 
 App.propTypes = {
-  currentView : PropTypes.string.isRequired
+  currentView: PropTypes.string.isRequired,
+  userInfo: PropTypes.object,
+  onClick: PropTypes.func,
+  isLoggedIn: PropTypes.func
 };
+
 
 export default App;
--- src/frontEnd/components/containers/App.js
+++ src/frontEnd/components/containers/App.js
@@ -1,12 +1,21 @@
 import {connect} from 'react-redux';
 import App from '../App.jsx';
+import {ChangeCurrentView,isLoggedIn}from '../../actions.js';
 
 const mapStateToProps = (state)=>{
   return{
-    currentView: state.currentView
+    currentView: state.currentView,
+    userInfo: state.userInfo
   };
 };
 
-const AppView= connect(mapStateToProps,null)(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClick: (currentView) =>{dispatch( ChangeCurrentView(currentView));},
+    isLoggedIn: () =>{ isLoggedIn();}
+  };
+};
+
+const AppView= connect(mapStateToProps,mapDispatchToProps)(App);
 
 export default AppView;
