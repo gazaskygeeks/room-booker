bb6353ee5ba24d5f0cfd80bf12f785b44ab2c61e
mhmd.shorafa <mhmd_shorafa@hotmail.com>
2017-05-04 09:36:09 +0000

    delete event then select new events and return them

--- src/backEnd/controllers/bookingsController.js
+++ src/backEnd/controllers/bookingsController.js
@@ -1,17 +1,8 @@
 const calenderOperations = require('../calendarapi/calenderutils.js');
 const {insertEvent,selectUserEvents} = require('../../database/reservation.js');
-const {setEvent} = require('../calendarapi/event.js');
 const jwt = require('../calendarapi/jwtauth.js');
 const dbuser = require('../../database/user.js');
 const calenderId = '1m5k20i4kuknts1fr7v7qql8v0@group.calendar.google.com';
-const dataInfo = {
-  summary: 'myTestSummary',
-  description: 'mytestDescription',
-  dateStart: '2017-05-02T20:30:00Z',
-  dateEnd: '2017-05-02T20:30:00Z',
-  organizerEmail: 'shahy.m.93@gmail.com',
-  organizerName: 'myTestOrganizerName'
-};
 
 module.exports = {
   getAllEvents: (req, res) => {
@@ -30,20 +21,17 @@ module.exports = {
         res.json(events);
       });
     });
-
   },
   UserEvent: (req,res)=>{
     selectUserEvents(req.body.email,(err,userEvent)=>{
       if (err)
         res.status(401).end();
       else {
-        res.status(200).json(userEvent).end();
+        res.json(userEvent);
       }
-
     });
   },
   createEvent: (req, res) => {
-    setEvent(dataInfo);
     jwt((err, jwtAuth) => {
       if (err) {
         res.status(300).json({
@@ -69,18 +57,25 @@ module.exports = {
     const calenderId = req.body;
     const eventId = req.body;
     jwt((err, jwtAuth) => {
+
       if (err) {
         res.status(300).json({
           'err': 'error in autherization'
         });
       }
-      calenderOperations.deleteEvent(jwtAuth, calenderId, eventId, (err, events) => {
+      calenderOperations.deleteEvent(jwtAuth, calenderId, eventId, (err) => {
         if (err) {
           res.status(300).json({
             'err': 'error deleting event'
           });
         }
-        res.json(events);
+        selectUserEvents(req.body.email,(err,userEvent)=>{
+          if (err)
+            res.status(401).end();
+          else {
+            res.json(userEvent);
+          }
+        });
       });
     });
   }
