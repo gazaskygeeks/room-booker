02d6ac1d92485857bc8f89c2e8cba6f3bbfb1314
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-07 14:07:06 +0300

    add mapStateToProps and return rooms as props

--- src/frontEnd/components/containers/Lobby.js
+++ src/frontEnd/components/containers/Lobby.js
@@ -2,14 +2,21 @@ import {connect} from 'react-redux';
 import Lobby from '../Lobby.jsx';
 import {ChangeCurrentView,getDayEvents,insertUser}from '../../actions.js';
 
+const mapStateToProps = (state)=>{
+  return({
+    rooms: state.rooms
+  });
+};
+
 const mapDispatchToProps = (dispatch) => {
   return {
     onClick: (currentView) =>{dispatch( ChangeCurrentView(currentView));},
     getEvent: ()=>{getDayEvents();},
-    logIn: (data) =>{insertUser(data);}
+    logIn: (data) =>{insertUser(data);},
+
   };
 };
 
-const LobbyView= connect(null,mapDispatchToProps)(Lobby);
+const LobbyView= connect(mapStateToProps,mapDispatchToProps)(Lobby);
 
 export default LobbyView;
