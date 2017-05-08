30623fad4677ba1acbc4b7ddddf59b964db25620
shahenaz Monia <shahy.m.93@gmail.com>
2017-05-08 12:21:11 +0300

    deleted cases if room availabitlity

--- src/frontEnd/components/App.jsx
+++ src/frontEnd/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Lobby from './containers/Lobby.js';
-import ChangeDayView from './containers/ChangeDayView.js';
+import RoomAvailability from './containers/RoomAvailability.js';
 import {PropTypes} from 'prop-types';
 import Home from './containers/Home.js';
 import Spinner from './Spinner.jsx';
@@ -21,14 +21,8 @@ class App extends React.Component {
 
   render(){
     switch (this.props.currentView) {
-    case 'ROOM_1_AVAILABILITY':
-      return <ChangeDayView room={this.props.currentView}/>;
-    case 'ROOM_2_AVAILABILITY':
-      return <ChangeDayView room={this.props.currentView}/>;
-    case 'ROOM_3_AVAILABILITY':
-      return <ChangeDayView room={this.props.currentView}/>;
-    case 'ROOM_4_AVAILABILITY':
-      return <ChangeDayView room={this.props.currentView}/>;
+    case 'ROOM_AVAILABILITY':
+      return <RoomAvailability />;
     case 'HOME':
       return <Home/>;
     case 'LOBBY':
@@ -45,7 +39,8 @@ App.propTypes = {
   currentView: PropTypes.string.isRequired,
   userInfo: PropTypes.object,
   onClick: PropTypes.func,
-  isLoggedIn: PropTypes.func
+  isLoggedIn: PropTypes.func,
+  getRooms : PropTypes.func
 };
 
 export default App;
