7a051be28891b5e9f0c8f90bc8e4796c3cbf882b
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-09 14:27:18 +0300

    remove getUserBookings call action

--- src/frontEnd/actions.js
+++ src/frontEnd/actions.js
@@ -34,11 +34,8 @@ const ChangeCurrentView = (currentView) => {
 };
 
 const getDayEvents = (id) => {
-
   fetch('/events/'+id).then(res => res.json()).then((result) => {
-
-
-    store.dispatch({type: 'FETCH_DAY_BOOKING', payload: formateEvents(result.items)});
+    store.dispatch({type: 'FETCH_DAY_BOOKING', payload: formateEvents(result)});
   }).catch((err) => {
         console.error('Error', err); //eslint-disable-line
   });
@@ -47,15 +44,14 @@ const getDayEvents = (id) => {
 
 };
 const createEvent = (event,id) => {
-  fetch('/event/'+id, {
+  fetch('/event/' + id, {
     method: 'POST',
-    body:JSON.stringify(event),
+    body: JSON.stringify(event),
     credentials:'include',
     headers: {
       'Accept': 'application/json',
       'content-type': 'application/json'
-    }})
-    .then(res => res.json()).then(() => {
+    }}).then(() => {
       getDayEvents(id);
     }).catch((err) => {
         console.error('Error', err); //eslint-disable-line
@@ -77,7 +73,6 @@ const isLoggedIn = () => {
     }
   }).then((result) => {
     store.dispatch({type: 'UPDATE_PROFILE', payload: result});
-    getUserBookings(result.email);
   }).catch((err) => {
         console.log(err); //eslint-disable-line
     store.dispatch({type: 'CHANGE_CURRENT_VIEW', payload: 'HOME'});
@@ -110,6 +105,8 @@ const formateEvents = (events) => {
   });
   return reformatEvents;
 };
+
+
 
 const logout = () => {
   fetch('/logout', {credentials: 'include'}).then((res) => {
