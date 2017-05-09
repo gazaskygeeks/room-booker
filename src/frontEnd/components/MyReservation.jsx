090cb6e5b2d2b73a30572cc6a979f5d810c13bd5
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-09 18:08:29 +0300

    convert the component to stateless

--- src/frontEnd/components/MyReservation.jsx
+++ src/frontEnd/components/MyReservation.jsx
@@ -1,47 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {PropTypes} from 'prop-types';
 
 
-class MyReservation extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentWillReceiveProps(){
-  }
-
-  getUserReservations() {
-    if (this.props.userReservations !== []) {
-      const {deleteEvent} = this.props;
-      return (
-        this.props.userReservations.map(function(event) {
-          return(  <div className="col-md-4 col-sm-6 col-xs-12" key={event.id}>
-          <div className="card">
-            <div className="card-block">
-              <h3 className="card-title">{event.summary}</h3>
-              <a className="btn btn-danger" onClick={deleteEvent(event.event_id,event.room_id)}>Cancel</a>
-            </div>
-          </div>
-        </div>
-          );
-        })
-      );
-    }else {
-      return(
-        <div className="alert alert-info col-md-4" style={{marginTop: '20px', padding:'0'}}>
-          <h4 style={{marginTop: '20px', marginLeft:'20px'}}><strong>Info!</strong> You don't have any bookings.</h4>
-        </div>
-      );
-    }
-  }
-  render() {
-    return (
+const MyReservation = ({deleteEvent,userReservations}) => {
+  return (
       <div className="row">
-        {this.getUserReservations()}
+        {
+          (userReservations.length !== 0)
+            ? (
+              userReservations.map(function(event) {
+                return(  <div className="col-md-4 col-sm-6 col-xs-12" key={event.id}>
+                <div className="card">
+                  <div className="card-block">
+                    <h3 className="card-title">{event.summary}</h3>
+                    <a className="btn btn-danger" onClick={deleteEvent(event.event_id,event.room_id)}>Cancel</a>
+                  </div>
+                </div>
+              </div>
+                );
+              })
+            )
+            :(
+              <div className="alert alert-info col-md-4" style={{marginTop: '20px', padding:'0'}}>
+                <h4 style={{marginTop: '20px', marginLeft:'20px'}}><strong>Info!</strong> You don't have any bookings.</h4>
+              </div>
+            )
+          }
       </div>
-    );
-  }
-}
+  );
+
+};
 
 MyReservation.propTypes = {
   userReservations: PropTypes.array,
