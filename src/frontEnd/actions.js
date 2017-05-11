4db45971c1e6b22d307959691aa81ec06e8b6b80
mhmd.shorafa <mhmd_shorafa@hotmail.com>
2017-05-12 00:27:23 +0300

    fix conflict

--- src/frontEnd/actions.js
+++ src/frontEnd/actions.js
@@ -20,9 +20,11 @@ const insertUser = (data) => {
     if (res.status === 200) {
       isLoggedIn();
       res.json();
+    }else {
+      store.dispatch({type: 'UPDATE_PROFILE', payload: {Error: 'Not authorize user'}});
     }
   }).then((result) => {
-    store.dispatch({type: 'AUTHORIZED_USER', payload: result});
+    store.dispatch({type: 'UPDATE_PROFILE', payload: result});
   }).catch((err) => {
     store.dispatch({type: 'UNAUTHORIZED_USER', payload: err});
   });
@@ -34,7 +36,15 @@ const ChangeCurrentView = (currentView) => {
 };
 
 const getDayEvents = (id) => {
-  fetch('/events/'+id).then(res => res.json()).then((result) => {
+  fetch('/events/'+id)
+  .then(res => {
+    if (res.status === 200) {
+      return res.json();
+    } else {
+      return Promise.reject(Error('day events error'));
+    }
+  })
+  .then((result) => {
     store.dispatch({type: 'FETCH_DAY_BOOKING', payload: formateEvents(result)});
   }).catch((err) => {
         console.error('Error', err); //eslint-disable-line
@@ -53,7 +63,8 @@ const createEvent = (event,id) => {
       'content-type': 'application/json'
     }}).then(() => {
       getDayEvents(id);
-    }).catch(() => {
+    }).catch((err) => {
+        console.error('Error', err); //eslint-disable-line
     });
 };
 
@@ -100,7 +111,6 @@ const isLoggedIn = () => {
   }).catch((err) => {
         console.log(err); //eslint-disable-line
     store.dispatch({type: 'CHANGE_CURRENT_VIEW', payload: 'HOME'});
-    store.dispatch({type: 'UPDATE_PROFILE', payload: {}});
   });
 };
 
