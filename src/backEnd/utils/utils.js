34c7eabacd564c48af64e6e10957b3103c9e254d
Saral Kaushik <saral.kaushik@vincuventas.com>
2018-01-17 14:09:46 +0200

    fix brackets

--- src/backEnd/utils/utils.js
+++ src/backEnd/utils/utils.js
@@ -1,37 +1,37 @@
 const fetch = require('node-fetch');
 
 const checkAuth = (accessToken) => {
-  return fetch('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + accessToken)
-    .then(res=>res.json())
-    .then(result => {
-      if (result.email) {
-        return result.email;
-      } else {
-        return false;
-      }
-    });
+    return fetch('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + accessToken)
+        .then(res => res.json())
+        .then(result => {
+            if (result.email) {
+                return result.email;
+            } else {
+                return false;
+            }
+        });
 };
 const validEmail = (email) => {
-  return email.includes('@gazaskygeeks.com') || email.includes('@mercycorps') || email === 'gazaskygeeks@gmail.com' || email.includes('@gmail.com') ;
+    return email.includes('@gazaskygeeks.com') || email.includes('@mercycorps') || email === 'gazaskygeeks@gmail.com' || email.includes('@gmail.com');
 };
 
-const checkEventAvailability = (roomEvents,eventStartAt,eventEndAt) => {
-  console.log('start time', eventStartAt.getTime(), 'end time', endEndAt.getTime());
-  if(eventStartAt.getTime() > (new Date().getTime())){
-    return roomEvents.filter(elem =>
-    (eventStartAt.getTime() < elem.end_date.getTime() && eventEndAt.getTime() > elem.start_date.getTime()).map((elem) => {
-        elem.end_time = elem.end_date.getTime();
-        elem.start_time = elem.start_date.getTime();
-        return elem;
-    });
-  );
-  }
-  else {
-    return [{summary:'choose time in future'}];
-  }
+const checkEventAvailability = (roomEvents, eventStartAt, eventEndAt) => {
+    console.log('start time', eventStartAt.getTime(), 'end time', endEndAt.getTime());
+    if (eventStartAt.getTime() > (new Date().getTime())) {
+        return roomEvents.filter(elem =>
+            (eventStartAt.getTime() < elem.end_date.getTime() && eventEndAt.getTime() > elem.start_date.getTime())).map((elem) => {
+            elem.end_time = elem.end_date.getTime();
+            elem.start_time = elem.start_date.getTime();
+            return elem;
+        });
+    } else {
+        return [{
+            summary: 'choose time in future'
+        }];
+    }
 };
 module.exports = {
-  checkAuth,
-  validEmail,
-  checkEventAvailability
+    checkAuth,
+    validEmail,
+    checkEventAvailability
 };
